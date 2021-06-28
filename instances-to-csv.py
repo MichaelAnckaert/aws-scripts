630cea8854a35de65da95318061bb7ce107159a5
Michael Anckaert <michael.anckaert@sinax.be>
2021-06-28 21:57:09 +0200

    Add region support to instances-to-csv.py

--- instances-to-csv.py
+++ instances-to-csv.py
@@ -26,17 +26,17 @@ INSTANCE_COLUMNS = [
 ]
 
 
-def get_instances(next_token="") -> List[str]:
+def get_instances(region: str, next_token: str = "") -> List[str]:
     """Get EC2 instances from the AWS API and return a list of instance information"""
     instances = []
-    client = boto3.client("ec2")
+    client = boto3.client("ec2", region_name=region)
     response = client.describe_instances(NextToken=next_token)
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
             instances.append([f[1](instance.get(f[0])) for f in INSTANCE_COLUMNS])
 
     if response.get("NextToken"):
-        instances.extend(get_instances(next_token=response["NextToken"]))
+        instances.extend(get_instances(region, next_token=response["NextToken"]))
 
     return instances
 
@@ -49,9 +49,9 @@ def write_csv(instances: List[str], filename: str):
             writer.writerow(instance)
 
 
-def run(filename: str):
+def run(filename: str, region: str = None):
     print("Retrieving EC2 instances...")
-    instances = get_instances()
+    instances = get_instances(region)
     print(f"Writing CSV file '{filename}'")
     write_csv(instances, filename)
     print("Finished!")
@@ -69,7 +69,13 @@ if __name__ == "__main__":
         type=str,
         help="Filename to write the output to",
     )
+    parser.add_argument(
+        "--region",
+        default=None,
+        type=str,
+        help="Region to collection instance information in",
+    )
 
     args = parser.parse_args()
 
-    run(args.filename)
+    run(region=args.region, filename=args.filename)
